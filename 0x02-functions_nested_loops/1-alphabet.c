cb2ae26e42b3914c77ef8768e987af17cdadc878
Nerdbull <abuulleonard@gmail.com>
2022-09-13 14:58:23 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Check the code
+ *
+ * Return: 0 Always Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
