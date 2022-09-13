40551116b65fbec475bf0f1ee48617f39da9ad26
Nerdbull <abuulleonard@gmail.com>
2022-09-13 16:04:28 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - Check the code
+ * print_alphabet - function to print abc
  *
- * Return: 0 Always Success
+ * Return: 0
  */
 void print_alphabet(void)
 {
-	char i;
+	char c;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
-	_putchar('\n');
+_putchar('\n');
 }
