23d72da612ff16aab7b11707f2916e5420d5ad08
Nerdbull <abuulleonard@gmail.com>
2022-09-13 15:00:35 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: 0 Always Success
  */
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
@@ -13,6 +13,4 @@ int main(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-
-	return (0);
 }
